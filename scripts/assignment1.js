97d932f8fd7b1d989b32cd7f5d55edc19a6f66d5
ericksoftware <ericklopezrl5qqq@gmail.com>
2025-02-25 17:02:25 -0800

    first commit

---
+++ scripts/assignment1.js
@@ -0,0 +1,43 @@
+// Funny Dinosaur Story
+
+// Let variables para permitir cambios en la historia
+let species = "T-Rex";
+let dream1 = "singer";
+let dream2 = "astronaut";
+let spaceship = "DinoRocket 3000";
+let microphone = "a banana";
+
+// Variables cambiables
+let name = "Rexy";
+let favoriteSong = "Dino Stomp";
+let planet = "Mars";
+
+// La historia divertida
+console.log(`Once upon a time, there was a ${species} named ${name}.`);
+console.log(`${name} had a big dream—to become a ${dream1} and an ${dream2} at the same time!`);
+console.log(`Every day, ${name} practiced singing with ${microphone}, hoping to perform on intergalactic stages.`);
+console.log(`But deep down, ${name} also wanted to explore the stars and visit ${planet}.`);
+console.log(`One day, ${name} built a spaceship called ${spaceship} using leftover pizza boxes and soda cans.`);
+console.log(`With a countdown of 3...2...1... BLAST OFF! ${name} soared into space while singing "${favoriteSong}" at the top of their lungs!`);
+console.log(`And so, ${name} became the first dinosaur to sing on ${planet}, proving that dreams (no matter how big or extinct) can come true!`);
+
+// Funny Dinosaur Story v2 
+
+// Cambio de valores sin errores
+species = "Cat";
+dream1 = "Dog";
+dream2 = "Cloud";
+spaceship = "CDC";
+microphone = "Hamster";
+name = "Winter";
+favoriteSong = "Cupid";
+planet = "Yuppy";
+
+// Nueva historia con los valores actualizados
+console.log(`Once upon a time, there was a ${species} named ${name}.`);
+console.log(`${name} had a big dream—to become a ${dream1} and an ${dream2} at the same time!`);
+console.log(`Every day, ${name} practiced singing with ${microphone}, hoping to perform on intergalactic stages.`);
+console.log(`But deep down, ${name} also wanted to explore the stars and visit ${planet}.`);
+console.log(`One day, ${name} built a spaceship called ${spaceship} using leftover pizza boxes and soda cans.`);
+console.log(`With a countdown of 3...2...1... BLAST OFF! ${name} soared into space while singing "${favoriteSong}" at the top of their lungs!`);
+console.log(`And so, ${name} became the first ${species} to sing on ${planet}, proving that dreams (no matter how big or extinct) can come true!`);
---
+++ scripts/variables.js
@@ -0,0 +1,47 @@
+console.log("Hello world");
+console.log("My name is " + "Erick" + " and I am " + 21 + " years old");
+
+let name = "Peter";
+let lastName = "Parker";
+console.log("Hello " + name + " " + lastName + ".");
+
+let age = 35;
+console.log(age); 
+
+let grade = 9.5;
+console.log(grade);
+
+let isApproved = true;
+console.log(isApproved);
+
+let number1 = 50;
+let number2 = 5;
+let addition = number1 + number2;
+
+
+// console.log(number1 + " plus " + number2 + " equals " + (number1 + number2));
+console.log(number1 + " plus " + number2 + " equals " + addition);
+
+number1 = 7;
+number2 = 3;
+let substraction = number1 - number2;
+console.log(number1 + " minus " + number2 + " equals " + substraction);
+
+let division = number1 / number2;
+console.log(number1 + " divided by " + number2 + " equals " + division.toFixed(2));
+
+let multiplication = number1 * number2;
+console.log(number1 + " multiplied by " + number2 + " equals " + multiplication);
+
+var x = 10;
+console.log(x);
+
+
+let y = 5;
+console.log(y);
+
+const z = 3;
+console.log(z);
+
+const a = "a";
+console.log(a);
